5d0b6566ce5fa9286d1a0f7592976bf21ef968f9
Uliana <mirrorrune21363@gmail.com>
2024-02-07 21:37:11 +0300

    add mainactivity behavior + debugging

--- app/src/main/java/com/example/mvi_app/MainActivity.kt
+++ app/src/main/java/com/example/mvi_app/MainActivity.kt
@@ -2,10 +2,105 @@ package com.example.mvi_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mvi_app.data.api.ApiHelperImpl
+import com.example.mvi_app.R
+import com.example.mvi_app.data.api.RetrofitBuilder
+import com.example.mvi_app.data.model.User
+import com.example.mvi_app.databinding.ActivityMainBinding
+import com.example.mvi_app.ui.main.adapter.MainAdapter
+import com.example.mvi_app.ui.main.intent.MainIntent
+import com.example.mvi_app.ui.main.viewmodel.MainViewModel
+import com.example.mvi_app.ui.main.viewstate.MainState
+import com.example.mvi_app.util.ViewModelFactory
+//import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@ExperimentalCoroutinesApi
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainViewModel: MainViewModel
+    private var adapter = MainAdapter(arrayListOf())
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupUI()
+        setupViewModel()
+        observeViewModel()
+        setupClicks()
+    }
+
+    private fun setupUI() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.run {
+            addItemDecoration(
+                DividerItemDecoration(
+                    binding.recyclerView.context,
+                    (binding.recyclerView.layoutManager as LinearLayoutManager).orientation
+                )
+            )
+        }
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun setupViewModel() {
+        mainViewModel = ViewModelProvider(
+            this,
+            ViewModelFactory(
+                ApiHelperImpl(
+                    RetrofitBuilder.apiService
+                )
+            )
+        ).get(MainViewModel::class.java)
+    }
+
+    private fun observeViewModel() {
+        lifecycleScope.launch {
+            mainViewModel.state.collect{
+                when(it) {
+                    is MainState.Idle -> {
+
+                    }
+                    is MainState.Loading -> {
+                        binding.buttonFetchUser.visibility = View.GONE
+                        binding.progressBar.visibility = View.VISIBLE
+                    }
+                    is MainState.Users -> {
+                        binding.progressBar.visibility = View.GONE
+                        binding.buttonFetchUser.visibility = View.GONE
+                        renderList(it.users)
+                    }
+                    is MainState.Error -> {
+                        binding.progressBar.visibility = View.GONE
+                        binding.buttonFetchUser.visibility = View.VISIBLE
+                        Toast.makeText(this@MainActivity, it.error,Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun renderList(users: List<User>) {
+        binding.recyclerView.visibility = View.VISIBLE
+        users.let {listOfUsers -> listOfUsers.let {adapter.addData(it)}}
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun setupClicks() {
+        binding.buttonFetchUser.setOnClickListener {
+            lifecycleScope.launch {
+                mainViewModel.usersIntent.send(MainIntent.FetchUser)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mvi_app/ui/main/adapter/MainAdapter.kt
+++ app/src/main/java/com/example/mvi_app/ui/main/adapter/MainAdapter.kt
@@ -7,26 +7,26 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.mvi_app.R
 import com.example.mvi_app.data.model.User
+import com.example.mvi_app.databinding.ItemLayoutBinding
 
 class MainAdapter(
     private val users: ArrayList<User>
 ) : RecyclerView.Adapter<MainAdapter.DataViewHolder>() {
 
-    class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class DataViewHolder(binding: ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+        val binding = binding
         fun bind(user: User) {
-            itemView.textViewUserName.text = user.name
-            itemView.textViewUserEmail.text = user.email
-            Glide.with(itemView.imageViewAvatar.context)
+            binding.textViewUserName.text = user.name
+            binding.textViewUserEmail.text = user.email
+            Glide.with(binding.imageViewAvatar.context)
                 .load(user.avatar)
-                .into(itemView.imageViewAvatar)
+                .into(binding.imageViewAvatar)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         DataViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.item_layout, parent, false
-            )
+            ItemLayoutBinding.inflate(LayoutInflater.from(parent.context))
         )
 
     override fun getItemCount(): Int = users.size
